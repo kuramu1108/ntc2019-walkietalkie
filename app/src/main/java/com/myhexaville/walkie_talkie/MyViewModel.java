721dcb02b2be9cfc9446fbf17803c540455a2c1b
kuramu1108 <kuramu1108u2@gmail.com>
2019-10-01 15:02:07 +0800

    improve ux and responsive ui

--- app/src/main/java/com/myhexaville/walkie_talkie/MyViewModel.java
+++ app/src/main/java/com/myhexaville/walkie_talkie/MyViewModel.java
@@ -12,7 +12,7 @@ public class MyViewModel extends ViewModel {
     public String talkerName = "";
     public Boolean clientRunning = false;
 
-    public MutableLiveData<Boolean> serverConnection = new MutableLiveData<Boolean>();
+    public MutableLiveData<Boolean> serverConnection = new MutableLiveData<>();
 
     public MutableLiveData<List<Talk>> talkHistory = new MutableLiveData<>();
 
@@ -23,5 +23,13 @@ public class MyViewModel extends ViewModel {
             else return "<text></text>";
         }
         else return "<text></text>";
+    }
+
+    public Boolean getServerConnection() {
+        if (serverConnection.getValue() != null) return serverConnection.getValue();
+        else {
+            serverConnection.setValue(false);
+            return false;
+        }
     }
 }
